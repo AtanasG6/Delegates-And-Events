58b75b4d6412c62d64d453448daf1a9dc7c8e119
AtanasG6 <gyulchev666@gmail.com>
2024-04-24 19:53:10 +0300

    An example that the delegate remembers which object the function is invoked from

--- Delegates And Events/Program.cs
+++ Delegates And Events/Program.cs
@@ -1,5 +1,16 @@
 ﻿namespace Delegates_And_Events
 {
+	class State
+	{
+		public delegate void StateDelegate();
+		public string InnerState { get; set; }
+
+		public void PrintState()
+		{
+			Console.WriteLine(InnerState);
+		}
+	}
+
 	public class Program
 	{
 		static void Main()
@@ -10,6 +21,14 @@
 			var example2 = new Example2();
 			//example2.TestDelegate();
 			example2.TestDelegate2();
+
+			State s1 = new State() {InnerState = "s1"};
+			State s2 = new State() { InnerState = "s2" };
+
+			var printState = new State.StateDelegate(s1.PrintState);
+
+			printState();
+
 			Console.ReadKey();
 		}
 	}
